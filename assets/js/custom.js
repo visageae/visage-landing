f7712ee1f37f07c61f38e96dca1b569a6b889f32
Adrien <adrien.barreau@gmail.com>
2016-08-18 17:49:20 +0200

    MENU Responsive : Y a encore un soucis de css quand le menu se replie le fond redevient sombre
    HEADER
    Logo Visage :
    Trop petit, si tu pouvais reduire les marges et utiliser le svg
    FOOTER
    Contacts : team@visage.jobs au lieu de info. Le texte a été modifié mais pas le lien
    
    HOME,
    Toujours le probleme de NaN au debut du scrolling, j ai reussi a faire un screen !Inline image 1
    Pour les temoignages clients tu peux reprendre ceux qui sont ici http://visage.jobs/
    Il manque le slider avec la liste des jobs.
    A priori ce n etait pas dans les moqups de depart.... mais est ce que tu pourrais rajouter de la meme maniere que la page actuelle (http://visage.jobs)  la liste des logos de nos clients en dessous la phrase "No bad apples here. We only work with top companies"
    Y a un exemple sur le site du template http://vivaco.com/demo/startuply/index-main.html
    RECRUITERS
    Il manque la section des testimonials des recruiters. Le business case "Meet Claudia" et les 3 testimonials en dessous (voir moqup)
    PRICING
    On a completement changé le pricing... je sais que c est pas prevu dans le devis de base mais tu me dis ce que tu as le temps de faire ou pas. Je pense que c est assez rapide, l'idée c'est de reprendre cette page : http://visage.jobs/pricing et de mettre en forme nos 3 box de la meme maniere que la : http://vivaco.com/demo/startuply/index-main.html#product enfin avec des plus petites box du coup.
    Du coup le bouton TRY IT serait en dessous chaque box
    Pour le formulaire qui apparait :
    Nom prenom, email, phone
    Nombre de jobs
    Average salary
    Location
    Est ce que tu as regardé pour integrer un formulaire Wufoo ? Ca serait plus facile pour nous d integrer ensuite
    CAREERS
    Je capte pas trop le delire lol, c est Guillaume qui t avait demandé ca ? On a pas encore le contenu a mettre la dedans donc laisse.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
 
     var counter = function() {
         var anim = function(elem, start, cb){
-            $(elem).animate({countNum: start}, {
+            $(elem).animate({countNum: parseInt(start)}, {
                 duration: 1500,
                 easing:'linear',
                 step: function() {
@@ -29,6 +29,56 @@
     $(document).on('ready', function () {
         counter();
     });
+
+    var isWufooContactInit = false;
+    $('#myModal').on('show.bs.modal', function (event) {
+        if(!isWufooContactInit) {
+            isWufooContactInit = true;
+            var z8kv0cc0ktfm9y;(function(d, t) {
+                var s = d.createElement(t), options = {
+                    'userName':'techvisage',
+                    'formHash':'z8kv0cc0ktfm9y',
+                    'autoResize':true,
+                    'height':'auto',
+                    'async':true,
+                    'host':'wufoo.com',
+                    'header':'show',
+                    'ssl':true};
+                s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+                s.onload = s.onreadystatechange = function() {
+                    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+                    try { z8kv0cc0ktfm9y = new WufooForm();z8kv0cc0ktfm9y.initialize(options);z8kv0cc0ktfm9y.display(); } catch (e) {}};
+                var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+            })(document, 'script');
+        }
+    });
+
+    var isWufooTryInit = false;
+    $('#tryModal').on('show.bs.modal', function (event) {
+        console.log("tryModal");
+        if(!isWufooTryInit) {
+            isWufooTryInit = true;
+            var m1fs4a3q0krw3pa;(function(d, t) {
+                var s = d.createElement(t), options = {
+                    'userName':'techvisage',
+                    'formHash':'m1fs4a3q0krw3pa',
+                    'autoResize':true,
+                    'height':'620',
+                    'async':true,
+                    'host':'wufoo.com',
+                    'header':'show',
+                    'ssl':true};
+                s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+                s.onload = s.onreadystatechange = function() {
+                    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+                    try { m1fs4a3q0krw3pa = new WufooForm();m1fs4a3q0krw3pa.initialize(options);m1fs4a3q0krw3pa.display(); } catch (e) {}};
+                var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+            })(document, 'script');
+        }
+    });
+
+
+
 
 })( jQuery );
 
--- assets/js/visageControllers.js
+++ assets/js/visageControllers.js
@@ -12,6 +12,7 @@ app.controller('homeController', ['$scope','$http',function($scope, $http) {
     $scope.jobs = [];
     $http.get("/assets/json/jobs.json").success(function (data, status) {
         $scope.jobs = data;
+        console.log($scope.jobs);
     });
 
 }]);
