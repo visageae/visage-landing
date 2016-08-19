9180dfac24b57b317b8abcb1a92d876291f35e68
Adrien <adrien.barreau@gmail.com>
2016-08-20 01:58:03 +0200

    Add close button

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,14 @@
-;(function($){
+;(function (d, $, t) {
 
-    var counter = function() {
-        var anim = function(elem, start, cb){
+    var counter = function () {
+        var anim = function (elem, start, cb) {
             $(elem).animate({countNum: parseInt(start)}, {
                 duration: 1500,
-                easing:'linear',
-                step: function() {
+                easing: 'linear',
+                step: function () {
                     $(this).text(parseInt(Math.floor(this.countNum)));
                 },
-                complete: function() {
+                complete: function () {
                     $(this).text(parseInt(this.countNum));
                     cb();
                 }
@@ -16,10 +16,10 @@
         };
 
 
-        $( ".counter-container .animated" ).on( "appear", function( event, param1, param2 ) {
-            $(event.target).find( ".counter" ).each(function(){
+        $(".counter-container .animated").on("appear", function (event, param1, param2) {
+            $(event.target).find(".counter").each(function () {
                 var start = $(this).text();
-                anim(this, start, function(){
+                anim(this, start, function () {
 
                 });
             });
@@ -30,55 +30,58 @@
         counter();
     });
 
-    var isWufooContactInit = false;
-    $('#myModal').on('show.bs.modal', function (event) {
-        if(!isWufooContactInit) {
-            isWufooContactInit = true;
-            var z8kv0cc0ktfm9y;(function(d, t) {
-                var s = d.createElement(t), options = {
-                    'userName':'techvisage',
-                    'formHash':'z8kv0cc0ktfm9y',
-                    'autoResize':true,
-                    'height':'auto',
-                    'async':true,
-                    'host':'wufoo.com',
-                    'header':'show',
-                    'ssl':true};
-                s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
-                s.onload = s.onreadystatechange = function() {
-                    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-                    try { z8kv0cc0ktfm9y = new WufooForm();z8kv0cc0ktfm9y.initialize(options);z8kv0cc0ktfm9y.display(); } catch (e) {}};
-                var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-            })(document, 'script');
-        }
-    });
 
-    var isWufooTryInit = false;
+    var z8kv0cc0ktfm9y;
+    var m1fs4a3q0krw3pa;
+    var s = d.createElement(t), options = {
+        'userName': 'techvisage',
+        'formHash': 'z8kv0cc0ktfm9y',
+        'autoResize': true,
+        'async': true,
+        'host': 'wufoo.com',
+        'ssl': true
+    };
+    s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+    s.onload = s.onreadystatechange = function () {
+        var rs = this.readyState;
+        if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+        try {
+            z8kv0cc0ktfm9y = new WufooForm();
+            z8kv0cc0ktfm9y.initialize(options);
+            z8kv0cc0ktfm9y.display();
+        } catch (e) {
+        }
+    };
+    var scr = d.getElementsByTagName(t)[0], par = scr.parentNode;
+    par.insertBefore(s, scr);
+
+    var sTry = d.createElement(t), optionstry = {
+        'userName': 'techvisage',
+        'formHash': 'm1fs4a3q0krw3pa',
+        'autoResize': true,
+        'async': true,
+        'host': 'wufoo.com',
+        'ssl': true
+    };
+    sTry.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+    sTry.onload = s.onreadystatechange = function () {
+        var rs = this.readyState;
+        if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+        try {
+            m1fs4a3q0krw3pa = new WufooForm();
+            m1fs4a3q0krw3pa.initialize(optionstry);
+            m1fs4a3q0krw3pa.display();
+        } catch (e) {
+        }
+    };
+    var scrTry = d.getElementsByTagName(t)[0];
+    var parTry = scrTry.parentNode;
+    parTry.insertBefore(sTry, scrTry);
+
+
     $('#tryModal').on('show.bs.modal', function (event) {
-        console.log("tryModal");
-        if(!isWufooTryInit) {
-            isWufooTryInit = true;
-            var m1fs4a3q0krw3pa;(function(d, t) {
-                var s = d.createElement(t), options = {
-                    'userName':'techvisage',
-                    'formHash':'m1fs4a3q0krw3pa',
-                    'autoResize':true,
-                    'height':'620',
-                    'async':true,
-                    'host':'wufoo.com',
-                    'header':'show',
-                    'ssl':true};
-                s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
-                s.onload = s.onreadystatechange = function() {
-                    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-                    try { m1fs4a3q0krw3pa = new WufooForm();m1fs4a3q0krw3pa.initialize(options);m1fs4a3q0krw3pa.display(); } catch (e) {}};
-                var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-            })(document, 'script');
-        }
+
     });
 
-
-
-
-})( jQuery );
+})(document, jQuery, 'script');
 
