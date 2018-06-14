a896fc69b47afde9c14fcf8e988727c566e951b2
Diego Romero <romero.pozo2012@gmail.com>
2018-06-13 21:49:08 -0500

    New lp rpo (#98)
    * WIP RPO
    
    * wip
    
    * rpo done
    
    * fix index and rpo coexist
    
    * fix config.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,108 +1,135 @@
 (function () {
-  var counter = function () {
-    var anim = function (elem, start, cb) {
-      $(elem).animate({countNum: parseInt(start)}, {
-        duration: 1500,
-        easing: 'swing',
-        step: function () {
-          var num = (this.countNum) ? this.countNum : 0;
-          $(this).text(parseInt(Math.floor(num)));
-        },
-        complete: function () {
-          $(this).text(parseInt(this.countNum));
-          cb();
-        }
-      });
+    var counter = function () {
+        var anim = function (elem, start, cb) {
+            $(elem).animate({countNum: parseInt(start)}, {
+                duration: 1500,
+                easing: 'swing',
+                step: function () {
+                    var num = (this.countNum) ? this.countNum : 0;
+                    $(this).text(parseInt(Math.floor(num)));
+                },
+                complete: function () {
+                    $(this).text(parseInt(this.countNum));
+                    cb();
+                }
+            });
+        };
+
+        $(".counter-container .animated").on("appear", function (event, param1, param2) {
+            $(event.target).find(".counter").each(function () {
+                var start = $(this).text();
+                anim(this, start, function () {
+
+                });
+            });
+        });
     };
 
-    $(".counter-container .animated").on("appear", function (event, param1, param2) {
-      $(event.target).find(".counter").each(function () {
-        var start = $(this).text();
-        anim(this, start, function () {
+    $(document).on('ready', function () {
+        counter();
 
+        $('#hireForm').on('submit', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            var email = $(this).find('#emailInput').val();
+            showModal(email);
+            return false;
+
+            function showModal(email) {
+                var q1gsxcg1150s39k;
+                var mail = email || '';
+                (function (d, t) {
+                    var s = d.createElement(t), options = {
+                        'userName': 'techvisage',
+                        'formHash': 'q1gsxcg1150s39k',
+                        'autoResize': true,
+                        'defaultValues': 'Field7=' + mail,
+                        'height': '677',
+                        'async': true,
+                        'host': 'wufoo.com',
+                        'header': 'hide',
+                        'ssl': true
+                    };
+                    s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+                    s.onload = s.onreadystatechange = function () {
+                        var rs = this.readyState;
+                        if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+                        try {
+                            q1gsxcg1150s39k = new WufooForm();
+                            q1gsxcg1150s39k.initialize(options);
+                            q1gsxcg1150s39k.display();
+                        } catch (e) {
+                        }
+                    };
+                    var scr = d.getElementsByTagName(t)[0], par = scr.parentNode;
+                    par.insertBefore(s, scr);
+                })(document, 'script');
+
+                $('#starterModal').modal('show');
+            }
         });
-      });
+
+        var transitionInterval = 2000;
+        setInterval(function () {
+            $('.team-quotes .team-quote:gt(0)').hide();
+            $('.team-quotes .team-quote:first')
+                .fadeOut(transitionInterval)
+                .next()
+                .fadeIn(transitionInterval)
+                .end()
+                .appendTo('.team-quotes');
+        }, 5000);
+        $(document).scroll(function (event) {
+            var windowTop = $(this).scrollTop();
+            $('#company').each(function () {
+                var objectTop = $(this).offset().top;
+                if (objectTop < windowTop) {
+                    $('#company .section-content').find('h1, .team-quotes').css('opacity', objectTop * 5 / windowTop)
+                } else {
+                    if ($('#company .section-content').find('h1, .team-quotes').css("opacity") == 0) {
+                        $('#company .section-content').find('h1, .team-quotes').fadeTo(200, 1);
+                    }
+                }
+            })
+        });
+        var $plan1 = $('.my_plan1');
+        var $plan2 = $('.my_plan2');
+
+        $plan1.on('mouseover', function () {
+            $plan1.addClass('my_plan_hovering')
+        });
+
+        $plan1.on('mouseleave', function () {
+            $plan1.removeClass('my_plan_hovering')
+        });
+
+        $plan2.on('mouseover', function () {
+            $plan2.addClass('my_plan_hovering')
+        });
+
+        $plan2.on('mouseleave', function () {
+            $plan2.removeClass('my_plan_hovering')
+        });
+
+        $(document).scroll(function (event) {
+            var windowTop = $(this).scrollTop();
+            $('#company').each(function () {
+                var objectTop = $(this).offset().top;
+                if (objectTop < windowTop) {
+                    $('#company .section-content').find('h1, .team-quotes').css('opacity', objectTop * 5 / windowTop)
+                } else {
+                    if ($('#company .section-content').find('h1, .team-quotes').css("opacity") == 0) {
+                        $('#company .section-content').find('h1, .team-quotes').fadeTo(200, 1);
+                    }
+                }
+            })
+        });
+        $('.c-bar-graph td').each(function () {
+            var max = 5000;
+            var cell = $(this).data('graph');
+            var cellHeight = (cell / max) * 100;
+            $('span', this).height(cellHeight + '%');
+        });
+        $('.counter').countTo();
     });
-  };
-
-  $(document).on('ready', function () {
-    counter();
-
-    $('#hireForm').on('submit', function(event){
-      event.preventDefault() ;
-      event.stopPropagation();
-      var email = $(this).find('#emailInput').val();
-      showModal(email);
-      return false;
-      function showModal(email){
-        var q1gsxcg1150s39k;
-        var mail = email || '';
-        (function(d, t) {
-          var s = d.createElement(t), options = {
-            'userName':'techvisage',
-            'formHash':'q1gsxcg1150s39k',
-            'autoResize':true,
-            'defaultValues': 'Field7=' + mail,
-            'height':'677',
-            'async':true,
-            'host':'wufoo.com',
-            'header':'hide',
-            'ssl':true
-          };
-          s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
-          s.onload = s.onreadystatechange = function() {
-            var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-            try { q1gsxcg1150s39k = new WufooForm();q1gsxcg1150s39k.initialize(options);q1gsxcg1150s39k.display(); } catch (e) {}
-          };
-          var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-        })(document, 'script');
-
-        $('#starterModal').modal('show');
-      }
-    });
-
-    var transitionInterval = 2000;
-    setInterval(function() {
-      $('.team-quotes .team-quote:gt(0)').hide();
-      $('.team-quotes .team-quote:first')
-        .fadeOut(transitionInterval)
-        .next()
-        .fadeIn(transitionInterval)
-        .end()
-        .appendTo('.team-quotes');
-    }, 5000);
-
-    $(document).scroll(function (event) {
-      var windowTop = $(this).scrollTop();
-      $('#company').each(function () {
-        var objectTop = $(this).offset().top;
-        if (objectTop < windowTop) {
-          $('#company .section-content').find('h1, .team-quotes').css('opacity', objectTop * 5  / windowTop)
-        } else {
-          if ($('#company .section-content').find('h1, .team-quotes').css("opacity") == 0) {
-            $('#company .section-content').find('h1, .team-quotes').fadeTo(200, 1);
-          }
-        }
-      })
-    });
-    var $plan1 = $('.my_plan1');
-    var $plan2 = $('.my_plan2');
-
-    $plan1.on('mouseover', function () {
-        $plan1.addClass('my_plan_hovering')
-    });
-
-    $plan1.on('mouseleave', function () {
-        $plan1.removeClass('my_plan_hovering')
-    });
-
-    $plan2.on('mouseover', function () {
-        $plan2.addClass('my_plan_hovering')
-    });
-
-    $plan2.on('mouseleave', function () {
-        $plan2.removeClass('my_plan_hovering')
-    });
-
-  });
 })();
